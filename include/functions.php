751cb5d5e3922b9b087942290ba0ded2aa023260
Chris Douglas <chrisgdouglas@gmail.com>
2020-03-01 01:48:04 -0600

    * Add curl function to handle options * Curl function returns result as json object

--- include/functions.php
+++ include/functions.php
@@ -98,6 +98,20 @@ function safe($value) {
   return $value;
 }
 
+function curl_api_get($url) {
+  $ch = curl_init();
+      curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+      curl_setopt($ch, CURLOPT_HEADER, 0);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);  
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 3);     
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      $json = json_decode(curl_exec($ch));
+      curl_close($ch);
+  return $json;
+}
+
 class UUID {
   public static function v4() {
     return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
